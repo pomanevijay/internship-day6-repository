599a4adc14bb46fef356c53fadacc722c59a7a32
pomanevijay <pomanevijay700@gmail.com>
2026-06-03 07:11:16 -0700

    internship day-6

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("hello from js!");
+
+let name = "vijay";
+console.log("Hey " + name + "!");
+
+let num1 = 10000;
+
+num1 = 5000;
+console.log(num1);
+
+const pi = 3.14;
+
+console.log(pi);
+
+console.log(typeof "name");
+console.log(typeof "num1");
+
+
+let number;
+console.log(number);
+
+let student_data = {
+    name: "vijay",
+    roll_no: 123,
+    class: "tyco",
+    division: "a",
+}
+
+console.log(student_data);
+
+let student = ["xyz", "pqr", "abc"];
+console.log(student);
+
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 != "9");
+console.log(1 <= "10");
